631d9e16c3dfd0b7873974c0619304de90cef7c6
Nino Marrazzo <nino.marrazzo@hinto.com>
2024-08-24 16:48:35 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,23 @@
 use std::io::{self, Write};
 
 fn main() {
-    // Uncomment this block to pass the first stage
-    print!("$ ");
-    io::stdout().flush().unwrap();
-
-    // Wait for user input
+    // Standard input handler
     let stdin = io::stdin();
     let mut command = String::new();
-    stdin.read_line(&mut command).unwrap();
-    command.pop();
 
-    println!("{}: command not found", command);
+    loop {
+        // Uncomment this block to pass the first stage
+        print!("$ ");
+        io::stdout().flush().unwrap();
+
+        command = String::new();
+        stdin.read_line(&mut command).unwrap();
+        command.pop();
+
+        if command.is_empty() || command.len() == 0 {
+            break;
+        }
+
+        println!("{}: command not found", command);
+    }
 }
