578cd97915f1f92bda92bade945bdea729684319
Nino Marrazzo <nino.marrazzo@hinto.com>
2024-08-31 12:45:55 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -9,10 +9,17 @@ use std::{
 };
 
 const ENV_PATH: &str = "PATH";
-const BUILTIN_CMD: [&str;3] = [
-    "echo",
-    "exit",
-    "type"
+
+const CMD_EXIT: &str = "exit";
+const CMD_TYPE: &str = "type";
+const CMD_ECHO: &str = "echo";
+const CMD_PWD: &str = "pwd";
+
+const BUILTIN_CMD: [&str;4] = [
+    CMD_EXIT,
+    CMD_ECHO,
+    CMD_TYPE,
+    CMD_PWD
 ];
 
 fn main() {
@@ -37,39 +44,54 @@ fn main() {
 
                 let command_parts: Vec<&str> = command.trim().split_whitespace().collect();
 
-                if command_parts[0] == "type" {
-                    if command_parts.len() > 1 {
-                        let command_name = command_parts[1];
-                        if BUILTIN_CMD.contains(&command_name) {
-                            println!("{} is a shell builtin", command_name);
+                match command_parts[0] {
+                    CMD_TYPE => {
+                        // Check if command_parts is greater then 1
+                        if command_parts.len() > 1 {
+                            // Obtain command name, second parameter passed to type command
+                            let command_name = command_parts[1];
+                            // Check if command_name is one of BUILTIN_CMD commands
+                            if BUILTIN_CMD.contains(&command_name) {
+                                println!("{} is a shell builtin", command_name);
+                            } else {
+                                // Otherwise locate program calling locate_program function
+                                match locate_program(command_name.to_string()) {
+                                    Some(path) => println!("{} is {}", command_name, path),
+                                    None => println!("{}: not found", command_name),
+                                }
+                            }
                         } else {
-                            match locate_program(command_name.to_string()) {
-                                Some(path) => println!("{} is {}", command_name, path),
-                                None => println!("{}: not found", command_name),
+                            println!("type: expected an argument");
+                        }
+                    },
+                    CMD_EXIT => {
+                        // Call exit with status code = 0
+                        std::process::exit(0);
+                    },
+                    CMD_PWD => {
+                        let current_dir = env::current_dir().unwrap();
+                        println!("{}", current_dir.display());
+                    },
+                    _ => {
+                        // If all other cases, find and execute the command
+                        match locate_program(command_parts[0].to_string()) {
+                            Some(command_path) => {
+                                // Get a slice starting from index 1 to the array's end
+                                let args = &command_parts[1..];
+                                let mut cmd = Command::new(command_path);
+
+                                if !args.is_empty() {
+                                    cmd.args(args);
+                                }
+
+                                let output = cmd.output().expect("Failed to execute command");
+                                io::stdout().write_all(&output.stdout).unwrap();
+                                io::stderr().write_all(&output.stderr).unwrap();
+                            },
+                            None => {
+                                println!("{}: command not found", command_parts[0]);
                             }
                         }
-                    } else {
-                        println!("type: expected an argument");
-                    }
-                } else if command_parts[0] == "exit" {
-                    std::process::exit(0);
-                } else {
-                    match locate_program(command_parts[0].to_string()) {
-                        Some(command_path) => {
-                            let args = &command_parts[1..];
-                            let mut cmd = Command::new(command_path);
-                            if !args.is_empty() {
-                                cmd.args(args);
-                            }
-
-                            let output = cmd.output().expect("Failed to execute command");
-                            io::stdout().write_all(&output.stdout).unwrap();
-                            io::stderr().write_all(&output.stderr).unwrap();
-                        }
-                        None => {
-                            println!("{}: command not found", command_parts[0]);
-                        }
-
                     }
                 }
             }
