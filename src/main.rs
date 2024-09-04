9bb0141a1ed41523f6e6bbf14c4f1b0417e53b7b
Nino Marrazzo <nino.marrazzo@hinto.com>
2024-09-04 17:28:27 +0200

    codecrafters submit [skip ci]

--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,76 @@
-use std::char::from_u32_unchecked;
 #[allow(unused_imports)]
 use std::env;
+use std::fmt;
 use std::format as f;
+use std::iter::FromIterator;
 use std::{
     fs,
     io::{self, Write},
     process::Command,
 };
 
+enum MyCommand {
+    Exit,
+    Type,
+    Echo,
+    Pwd,
+    Cd,
+}
+
+struct MyCommandContainer {
+    commands: Vec<MyCommand>,
+}
+
+impl MyCommandContainer {
+    fn new() -> Self {
+        MyCommandContainer {
+            commands: Vec::new(),
+        }
+    }
+
+    fn as_refs(&self) -> Vec<&MyCommand> {
+        self.commands.iter().collect()
+    }
+}
+
+impl MyCommand {
+    fn from_str(command: &str) -> Option<MyCommand> {
+        match command {
+            "exit" => Some(MyCommand::Exit),
+            "type" => Some(MyCommand::Type),
+            "echo" => Some(MyCommand::Echo),
+            "pwd" => Some(MyCommand::Pwd),
+            "cd" => Some(MyCommand::Cd),
+            _ => None,
+        }
+    }
+}
+
+impl fmt::Display for MyCommand {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            MyCommand::Cd => write!(f, "cd"),
+            MyCommand::Pwd => write!(f, "pwd"),
+            MyCommand::Exit => write!(f, "exit"),
+            MyCommand::Type => write!(f, "type"),
+            MyCommand::Echo => write!(f, "echo"),
+        }
+    }
+}
+
+impl<'a> FromIterator<&'a str> for MyCommandContainer {
+    fn from_iter<T: IntoIterator<Item = &'a str>>(iter: T) -> Self {
+        let mut container = MyCommandContainer::new();
+        for s in iter {
+            if let Some(cmd) = MyCommand::from_str(s) {
+                container.commands.push(cmd)
+            }
+        }
+        container
+    }
+}
+
 const ENV_PATH: &str = "PATH";
-
-const CMD_EXIT: &str = "exit";
-const CMD_TYPE: &str = "type";
-const CMD_ECHO: &str = "echo";
-const CMD_PWD: &str = "pwd";
-
-const BUILTIN_CMD: [&str;4] = [
-    CMD_EXIT,
-    CMD_ECHO,
-    CMD_TYPE,
-    CMD_PWD
-];
 
 fn main() {
     // Standard input handler
@@ -44,15 +94,15 @@ fn main() {
 
                 let command_parts: Vec<&str> = command.trim().split_whitespace().collect();
 
-                match command_parts[0] {
-                    CMD_TYPE => {
+                match MyCommand::from_str(command_parts[0]) {
+                    Some(MyCommand::Type) => {
                         // Check if command_parts is greater then 1
                         if command_parts.len() > 1 {
                             // Obtain command name, second parameter passed to type command
                             let command_name = command_parts[1];
                             // Check if command_name is one of BUILTIN_CMD commands
-                            if BUILTIN_CMD.contains(&command_name) {
-                                println!("{} is a shell builtin", command_name);
+                            if let Some(builtin_command) = MyCommand::from_str(command_name) {
+                                println!("{} is a shell builtin", builtin_command.to_string());
                             } else {
                                 // Otherwise locate program calling locate_program function
                                 match locate_program(command_name.to_string()) {
@@ -63,15 +113,25 @@ fn main() {
                         } else {
                             println!("type: expected an argument");
                         }
-                    },
-                    CMD_EXIT => {
+                    }
+                    Some(MyCommand::Exit) => {
                         // Call exit with status code = 0
                         std::process::exit(0);
-                    },
-                    CMD_PWD => {
+                    }
+                    Some(MyCommand::Pwd) => {
                         let current_dir = env::current_dir().unwrap();
                         println!("{}", current_dir.display());
-                    },
+                    }
+                    Some(MyCommand::Cd) => {
+                        if command_parts.len() > 0 {
+                            match std::env::set_current_dir(command_parts[1]) {
+                                Err(_) => {
+                                    println!("cd: {}: No such file or directory", command_parts[1])
+                                }
+                                Ok(_) => (),
+                            }
+                        }
+                    }
                     _ => {
                         // If all other cases, find and execute the command
                         match locate_program(command_parts[0].to_string()) {
@@ -87,7 +147,7 @@ fn main() {
                                 let output = cmd.output().expect("Failed to execute command");
                                 io::stdout().write_all(&output.stdout).unwrap();
                                 io::stderr().write_all(&output.stderr).unwrap();
-                            },
+                            }
                             None => {
                                 println!("{}: command not found", command_parts[0]);
                             }
